da68ca0fb1b8d63349182e00e814235b886087f8
killian2301 <killianjimenez@gmail.com>
2023-04-04 08:48:08 +0100

    first commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.spec.ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
---
+++ src/Kata1-Fibonacci/fibonacci.spec.ts
@@ -0,0 +1,56 @@
+
+describe("Fibonacci", () => {
+  it("should return 0 if receives 0", () => {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+    
+  });
+
+  it("should return 1 if receives 1", () => {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+
+  });
+
+  it("should return 1 if receives 2", () => {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+
+  });
+
+  it("should return 2 if receives 3", () => {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+
+  });
+
+  it("should return 3 if receives 4", () => {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+
+  });
+
+  it("should return 144 if receives 12", () => {
+    //ARRANGE
+
+    //ACT
+
+    //ASSERT
+
+  });
+});
